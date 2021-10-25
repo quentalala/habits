8d18bfb83b5816210331a5869546334ad34f23ba
Quentin Chin <q.chin@hotmail.com>
2021-10-24 21:37:17 -0400

    Add functions to interact with json-server

--- src/services/toBackend.jsx
+++ src/services/toBackend.jsx
@@ -1,1 +1,27 @@
 import axios from "axios";
+const baseUrl = "http://localhost:3001/habits";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(baseUrl, newObject);
+  console.log(request);
+  return request.then((response) => response.data);
+};
+
+const deleteEntry = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+const accessHabits = { getAll, create, deleteEntry, update };
+
+export default accessHabits;
